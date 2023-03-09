aef54a5348319b6c3f408c2e1ca2b26d2888c163
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2023-03-09 19:06:34 +0500

    deleted:    HomeWork_1/task_1.py 	deleted:    HomeWork_1/task_2.py 	deleted:    HomeWork_1/task_3.py 	deleted:    HomeWork_1/task_4.py 	deleted:    Home_Work_2/task_10.py 	deleted:    Home_Work_2/task_12.py 	deleted:    Home_Work_2/task_14.py 	deleted:    Home_work3/task_16.py 	deleted:    Home_work3/task_18.py 	deleted:    Home_work3/task_20.py 	deleted:    Home_work4/task_22.py 	deleted:    Home_work4/task_24.py

--- HomeWork_1/task_4.py
+++
@@ -1,8 +0,0 @@
-x = int(input('Введите длинну плитки: '))
-y = int(input('Введите ширину плитки: '))
-k = int(input('Сколько долек хотите отломить: '))
-
-if 1 <= k < y * x and (k % x == 0 or k % y == 0):
-    print('Yes')
-else:
-    print('No')
