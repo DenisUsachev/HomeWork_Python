85707c7cfd1335088461ebb3e43171684869ba25
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2023-03-09 17:49:16 +0500

    Add new Home Work 5

--- Home_work3/task_24.py
+++ Home_work3/task_24.py
@@ -9,18 +9,17 @@
 import random
 n =int(input('Введите количество растений: '))
 arr = [random.randrange(10) for _ in range(n)]
+
 print(arr)
 summ = 0
 i = 0
 while i > n - 3:
     if summ < arr[i] + arr[i + 1] + arr[i + 2]:
         summ = arr[i] + arr[i + 1] + arr[i + 2]
-        print(summ)
     i += 1
 while i != n :
     if summ < arr[i - n] + arr[i + 1 - n] + arr[i + 2 - n]:
         summ = arr[i - n] + arr[i + 1 - n] + arr[i + 2 - n]
-        print(summ)
     i += 1
 
 
